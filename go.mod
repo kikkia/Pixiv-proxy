d9a9928af7d15b4bbee6a225d49c3524b0d09aa0
kikkia <sabledawn@gmail.com>
2022-03-23 01:26:00 -0700

    add initial service

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kikkia/pixiv-proxy
+
+go  1.18
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "log"
+	"io/ioutil"
+    "net/http"
+)
+
+var pixiv_domain = "https://i.pximg.net/"
+var client = &http.Client{}
+
+func main() {
+	handler := http.HandlerFunc(Serve)
+	http.ListenAndServe(":8080", handler)
+}
+
+func Serve(w http.ResponseWriter, r *http.Request) {
+	url := pixiv_domain + r.URL.Path
+    req, _ := http.NewRequest("GET", url, nil)
+	// In order to use the image link the referer header must be set
+	req.Header.Set("referer", "https://www.pixiv.net/")
+	res, _ := client.Do(req)
+
+	// Ream image data
+	data, err := ioutil.ReadAll(res.Body)
+
+    if err != nil {
+        log.Fatalf("ioutil.ReadAll -> %v", err)
+    }
+	res.Body.Close()
+    
+	// Map headers over to new response 
+	for key, header := range res.Header {
+		w.Header().Set(key, header[0])
+	}
+	w.WriteHeader(res.StatusCode)
+
+	w.Write(data)
+	return
+}
