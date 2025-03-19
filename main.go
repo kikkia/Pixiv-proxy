d414a1cc9c32c722198175d2c3533706f6d654ef
kikkia <10608318+kikkia@users.noreply.github.com>
2025-03-20 00:43:27 +0900

    cleanup and better error handling

--- main.go
+++ main.go
@@ -12,34 +12,43 @@ var client = &http.Client{}
 
 func main() {
 	handler := http.HandlerFunc(Serve)
-	port := getenv("SERVER_PORT", "80")
-	http.ListenAndServe(":"+port, handler)
-	log.Printf("Started server on port " + port)
+    port := getenv("SERVER_PORT", "80")
+    log.Printf("Starting server on port " + port)
+    log.Fatal(http.ListenAndServe(":"+port, handler))
 }
 
 func Serve(w http.ResponseWriter, r *http.Request) {
 	url := pixiv_domain + r.URL.Path
-	req, _ := http.NewRequest("GET", url, nil)
-	// In order to use the image link the referer header must be set
-	req.Header.Set("referer", "https://www.pixiv.net/")
-	res, _ := client.Do(req)
-
-	// Ream image data
-	data, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		log.Fatalf("ioutil.ReadAll -> %v", err)
-	}
-	res.Body.Close()
-
-	// Map headers over to new response
-	for key, header := range res.Header {
-		w.Header().Set(key, header[0])
-	}
-	w.WriteHeader(res.StatusCode)
-
-	w.Write(data)
-	return
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        http.Error(w, "Error creating request: "+err.Error(), http.StatusInternalServerError)
+        log.Printf("Error creating request: %v", err)
+        return
+    }
+    
+    // In order to use the image link the referer header must be set
+    req.Header.Set("referer", "https://www.pixiv.net/")
+    res, err := client.Do(req)
+    if err != nil {
+        http.Error(w, "Error making request: "+err.Error(), http.StatusInternalServerError)
+        log.Printf("Error making request to %s: %v", url, err)
+        return
+    }
+    
+    defer res.Body.Close()
+    
+    data, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        http.Error(w, "Error reading response: "+err.Error(), http.StatusInternalServerError)
+        log.Printf("Error reading response: %v", err)
+        return
+    }
+    
+    for key, header := range res.Header {
+        w.Header().Set(key, header[0])
+    }
+    w.WriteHeader(res.StatusCode)
+    w.Write(data)
 }
 
 func getenv(key, fallback string) string {
